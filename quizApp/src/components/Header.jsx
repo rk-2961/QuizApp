b923d763d60f4d088ee3f57ad57097000e8229fd
Rahul Khandelwal <rahul.khandelwal@atmecs.com>
2024-05-22 16:19:56 +0530

    Created two components

---
+++ quizApp/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import logo from  '../assets/quiz-logo.png'
+export default function Header(){
+    return(
+        <>
+         <header>
+            <img src={logo} alt="Quiz Logo" />
+            <h1 >ReactQuiz</h1>
+            
+        </header>
+        </>
+    )
+}
